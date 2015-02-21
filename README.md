81c440914a93ee6ae62dbe39b6272e919ef34b54
Simon Sapin <simon.sapin@exyr.org>
2015-02-21 08:07:25 +0100

    Import Selectors-related files from Servo.
    I did not preserve the commit history. Look in the Servo repository
    from d26345f868e4261760ce204bdc383941e8f14621 and back for that.
    
    Imported files are:
    
    components/style/selectors.rs -> src/parser.rs
    components/style/selector_matching.rs -> src/matching.rs
    components/style/node.rs -> src/tree.rs
    components/util/bloom.rs -> src/bloom.rs
    components/util/sort.rs -> src/quicksort.rs
    components/util/smallvec.rs -> src/smallvec.rs
    
    This library currently targets rustc 80627cd3c 2015-02-07,
    the version currently used by Servo, which is a few weeks behind Rust Nightly.
    Against convention for libraries, Cargo.lock is included in the repository
    since it was tricky to get right:
    many dependencies have been upgraded for furter breaking changes
    in the language or standard library,
    so we need a version that is not the latest.

---
+++ README.md
@@ -0,0 +1,6 @@
+rust-selectors
+==============
+
+A CSS Selectors matching library extracted
+from [Servo](https://github.com/servo/servo)
+at commit [d326e75](https://github.com/servo/servo/tree/d26345f868e4261760ce204bdc383941e8f14621/components/style).
---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#![feature(box_syntax, int_uint, plugin)]
+
+#[macro_use] extern crate bitflags;
+#[macro_use] extern crate cssparser;
+#[macro_use] extern crate matches;
+extern crate string_cache;
+#[no_link] #[macro_use] #[plugin] extern crate string_cache_macros;
+
+//mod bloom;
+//mod matching;
+//mod parser;
+//mod tree;
