027ca26a91540c1cd2bdcfa01c88361a5c7ccdc8
Simon Sapin <simon.sapin@exyr.org>
2015-04-05 22:14:44 +0200

    Add a quicksort<T: Ord>() function. Fix #11.

--- src/quicksort.rs
+++ src/quicksort.rs
@@ -67,6 +67,7 @@ where F: Fn(&T, &T) -> Ordering {
     quicksort_helper(arr, i, right, compare);
 }
 
+
 /// An in-place quicksort.
 ///
 /// The algorithm is from Sedgewick and Bentley, "Quicksort is Optimal":
@@ -80,12 +81,20 @@ pub fn quicksort_by<T, F>(arr: &mut [T], compare: F) where F: Fn(&T, &T) -> Orde
     quicksort_helper(arr, 0, (len - 1) as isize, &compare);
 }
 
+
+/// An in-place quicksort for ordered items.
+#[inline]
+pub fn quicksort<T>(arr: &mut [T]) where T: Ord {
+    quicksort_by(arr, |a, b| a.cmp(b))
+}
+
+
 #[cfg(test)]
 pub mod test {
     use rand;
     use rand::Rng;
 
-    use super::quicksort_by;
+    use super::quicksort;
 
     #[test]
     pub fn random() {
@@ -93,7 +102,7 @@ pub mod test {
         for _ in 0u32 .. 50000u32 {
             let len: usize = rng.gen();
             let mut v: Vec<isize> = rng.gen_iter::<isize>().take((len % 32) + 1).collect();
-            quicksort_by(&mut v, |a, b| a.cmp(b));
+            quicksort(&mut v);
             for i in 0 .. v.len() - 1 {
                 assert!(v[i] <= v[i + 1])
             }
