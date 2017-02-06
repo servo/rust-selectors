7c2387c32bb9ab53b63e877e7bfad2992f10d752
Simon Sapin <simon.sapin@exyr.org>
2017-02-06 21:52:27 +0100

    Remove heapsize feature
    It’s broken on current Rust versions (still uses the old compiler
    plugin) it nobody seems to use it since nobody noticed.

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#![cfg_attr(feature = "heap_size", feature(plugin, custom_derive))]
-#![cfg_attr(feature = "heap_size", plugin(heapsize_plugin))]
-
-#[cfg(feature = "heap_size")] extern crate heapsize;
 #[macro_use] extern crate bitflags;
 #[macro_use] extern crate cssparser;
 #[macro_use] extern crate matches;
